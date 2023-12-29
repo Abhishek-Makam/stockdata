dc5ac7110bac39b004fef000f23260a9aa80d71b
abhijuu <makamabhishek9802@gmail.com>
2023-12-29 14:13:34 +0530

    added log details

--- src/main/java/com/example/demo1/controller.java
+++ src/main/java/com/example/demo1/controller.java
@@ -4,9 +4,10 @@ import com.example.demo1.stock;
 import com.example.demo1.service.stockservice;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,10 +16,21 @@ import java.util.List;
 @RequestMapping("/stocks")
 @AllArgsConstructor
 public class controller {
-    private final stockservice stockservice;
+
+    private static final Logger logger = LoggerFactory.getLogger(controller.class);
+
+    private final stockservice stockService;
+
     @GetMapping("/getStockData")
-    public List<stock> getStockData() throws Exception
-    {
-        return stockservice.getCompanyData();
+    public List<stock> getStockData() {
+        try {
+            List<stock> stockData = stockService.getCompanyData();
+            logger.info("Retrieved stock data: {}", stockData);
+            return stockData;
+        } catch (Exception e) {
+            logger.error("Error while retrieving stock data", e);
+            // Handle the exception or rethrow it based on your requirements
+            throw new RuntimeException("Error while retrieving stock data", e);
+        }
     }
 }
